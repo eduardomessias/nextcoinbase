4b64193a898d8c51a220fac5c5b53748f90a42c7
eduardomessias <eduardo.messias@outlook.com>
2022-03-28 20:46:05 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 import '../styles/globals.css'
 
+
+const supportedChainsIds = [4] //[rinkeby]
+const connectors = {
+  injected: {},
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider connectors={connectors} supportedChainIds={supportedChainsIds}>
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
